37ee683a4ff6bd4d924391cfbf5c01a396ba569c
Povilas Kirna <povilas.kirna@gmail.com>
2022-10-22 18:46:19 +0200

    working on scrolling animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,33 @@
 export default function Home() {
+	function scrollDown(pages: number) {
+		if (
+			document.getElementById("0").offsetWidth > 0 &&
+			document.getElementById("0").offsetHeight > 0
+		) {
+			document.getElementById("1").scrollIntoView({ behavior: "smooth" });
+		}
+	}
+
 	return (
-		<div className="w-screen h-screen flex flex-col">
-			<div className="bg-pink-200">First</div>
-			<div className="bg-blue-200">Second</div>
-		</div>
+		<>
+			<div className="w-screen h-screen" id="0">
+				<div className="text-5xl font-bold text-center h-4/5 flex flex-row items-center justify-center">
+					Who doesn't like
+					<span>&nbsp;magic</span>?
+				</div>
+				<div
+					className="box flex flex-col justify-center items-center h-20"
+					onClick={() => {
+						scrollDown(1);
+					}}
+				>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+			<div className="w-screen h-screen" id="1">
+				Page 2
+			</div>
+		</>
 	);
 }
