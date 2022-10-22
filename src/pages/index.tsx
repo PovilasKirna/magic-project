e2aa60faba90d2fc6c5c02ca5779e27c69f4bd5e
Povilas Kirna <povilas.kirna@gmail.com>
2022-10-22 16:40:22 +0200

    First commit setup a project with tailwindcss, typescript, next

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<div className="w-screen h-screen flex flex-col">
+			<div className="bg-pink-200">First</div>
+			<div className="bg-blue-200">Second</div>
+		</div>
+	);
+}
